d9d9e8d5cf7affbee8f42db0cd1a4843f548891f
ayu2112 <2422500077@mahasiswa.atmaluhur.ac.id>
2026-09-23 17:46:34 +0700

    Upload projek CodeIgniter 3

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+    public function index() {
+        echo "<h1>SELAMAT DATANG DI WEBSITE KAMI</h1>";
+    }
+}
